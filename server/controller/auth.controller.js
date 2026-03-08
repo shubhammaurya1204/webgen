1f972c45094ed364ed4cfb6e456503b06843ca66
shubhammaurya1204 <shubhammaurya1204@gmail.com>
2026-03-08 12:23:34 +0530

    Update auth.controller.js

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -18,8 +18,8 @@ export const googleAuth = async(req,res)=>{
         // cookie created [for store a token]
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         })
         return res.status(200).json(user)
@@ -34,8 +34,8 @@ export const logOut = async(req,res)=>{
     try {
         res.clearCookie("token",{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict"
+            secure:true,
+            sameSite:"none"
         })
         return res.status(200).json({message:"Log out Successfully"})
     } catch (error) {
